7bcdaeb335bbe2c1d7cd6102f2aaf08c6dcac458
Manu Ioan Daniel <dreamteampro23@gmail.com>
2024-12-17 20:26:00 +0200

    .

---
+++ Facultate/Anul 1/Sem 1/FP/Lab 7-9/sortare/sort.py
@@ -0,0 +1,26 @@
+"""merge sort"""
+def my_sort(list,key=None,reverse=False):
+    if key is None:
+        key=lambda x:x
+    if len(list)<=1:
+        return list
+    mid=len(list)//2
+    left=my_sort(list[:mid],key,reverse)
+    right=my_sort(list[mid:],key,reverse)
+    return merge(left,right,key,reverse)
+def merge(left,right,key,reverse):
+    result=[]
+    i=j=0
+    while i<len(left) and j<len(right):
+        if (key(left[i])<=key(right[j])) != reverse:
+            result.append(left[i])
+            i+=1
+        else:
+            result.append(right[j])
+            j+=1
+    result.extend(left[i:])
+    result.extend(right[j:])
+    return result
+
+
+
