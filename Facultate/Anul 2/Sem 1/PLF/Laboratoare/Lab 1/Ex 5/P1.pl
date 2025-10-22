935b4358c9d5f246cefa50d8109adde0c5431c67
Manu Ioan Daniel <dreamteampro23@gmail.com>
2025-10-23 00:46:33 +0300

    .

--- Facultate/Anul 2/Sem 1/PLF/Laboratoare/Lab 1/Ex 5/P1.pl
+++ Facultate/Anul 2/Sem 1/PLF/Laboratoare/Lab 1/Ex 5/P1.pl
@@ -1,11 +1,11 @@
 %sterge(L:lista,E:element,Rez:lista)
 %model de flux (i,i,o)
-sterge(L,E,Rez):-L=[],Rez=[].
+sterge([],_,[]).
 sterge(L,E,Rez):-L=[H|T],H=E,sterge(T,E,Rez).
 sterge(L,E,Rez):-L=[H|T],H\=E,sterge(T,E,Rez2),Rez=[H|Rez2].
 %nrAparitii(L:lista,E:element,Rez:intreg)
 %model de flux(i,i,o)
-nrAparitii([],E,0).
+nrAparitii([],_,0).
 nrAparitii([H|T],E,Rez):-
     H=E,
     nrAparitii(T,E,Rez2),
@@ -17,7 +17,7 @@ nrAparitii([H|T],E,Rez):-
 %model de flux(i,o)
 listaPerechi([],[]).
 listaPerechi(L,Rez):-
-    L=[H|T],
+    L=[H|_],
     nrAparitii(L,H,NrAp),
     sterge(L,H,L2),
     listaPerechi(L2,Rez2),
