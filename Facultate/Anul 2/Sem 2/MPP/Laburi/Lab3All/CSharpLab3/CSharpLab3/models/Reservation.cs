67c627a584ad68a3ae3dca8723a5f609e1119832
Manu Ioan Daniel <dreamteampro23@gmail.com>
2026-03-19 15:52:16 +0200

    .

---
+++ Facultate/Anul 2/Sem 2/MPP/Laburi/Lab3All/CSharpLab3/CSharpLab3/models/Reservation.cs
@@ -0,0 +1,13 @@
+﻿namespace CSharpLab3.models;
+
+public class Reservation : Entity<long>
+{
+    public required string ClientName { get; init; }
+    public required int ReservedSeatsCount { get; init; }
+    public required long RideId { get; init; }
+
+    public override string ToString()
+    {
+        return $"Reservation: Client Name = {ClientName}, Reserved Seats Count = {ReservedSeatsCount}, Ride ID = {RideId}";
+    }
+}
---
+++ Facultate/Anul 2/Sem 2/MPP/Laburi/Lab3All/abc/CSharpLab_3/utils/DbUtils.cs
@@ -0,0 +1,41 @@
+﻿using MySql.Data.MySqlClient;
+using NLog;
+
+namespace CSharpLab_3.utils;
+
+public class DbUtils(string connectionString)
+{
+    private MySqlConnection? _connection;
+    private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+
+    private MySqlConnection GetNewConnection()
+    {
+        try
+        {
+            var con = new MySqlConnection(connectionString);
+            con.Open();
+            Logger.Info("Database connection established");
+            return con;
+        }
+        catch (MySqlException ex)
+        {
+            Logger.Error(ex, "Error creating database connection");
+            Console.WriteLine($"Error getting connection: {ex.Message}");
+            throw;
+        }
+    }
+
+    
+    public MySqlConnection GetConnection()
+    {
+        Logger.Info("Connecting to database...");
+        if (_connection is not { State: System.Data.ConnectionState.Open })
+        {
+            _connection = GetNewConnection();
+        }
+        return _connection;
+    }
+
+
+}
