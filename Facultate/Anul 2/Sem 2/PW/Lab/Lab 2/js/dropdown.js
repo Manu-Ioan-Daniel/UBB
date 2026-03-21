d13c87b854421f63120a7a31605e69d9f8f38127
Manu Ioan Daniel <dreamteampro23@gmail.com>
2026-03-21 19:28:45 +0200

    .

---
+++ Facultate/Anul 2/Sem 2/PW/Lab/Lab 2/js/dropdown.js
@@ -0,0 +1,12 @@
+
+
+document.querySelectorAll('.menu li').forEach(function(li) {
+    li.addEventListener('click', function() {
+        if (window.innerWidth <= 600) {
+            const dropdown = this.querySelector('.dropdown');
+            if (dropdown) {
+                dropdown.style.display === 'block' ? dropdown.style.display = 'none' : dropdown.style.display = 'block';
+            }
+        }
+    });
+});
